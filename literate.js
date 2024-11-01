71e411a049cde36f177b44af19865d539ed6d30b
shuiruge <shuiruge@whu.edu.cn>
2024-11-01 18:35:44 +0800

    Rearrange the material and code. Add style. Find that highlight.js cannot work well. It also highlights what is hidden (the unwoven element).

--- literate.js
+++ literate.js
@@ -6,7 +6,7 @@ function weaveChunkRef(chunkRef) {
   unwoven.setAttribute("class", "unwoven");
   unwoven.setAttribute("hidden", true);
   unwoven.innerHTML = chunkRef.innerHTML;
-  /* replace HTML */
+  /* Replace HTML */
   var chunkName = chunkRef.innerHTML;
   chunkRef.innerHTML = null;
   chunkRef.appendChild(document.createTextNode(`⟨${chunkName}⟩`));
@@ -14,11 +14,13 @@ function weaveChunkRef(chunkRef) {
 }
 
 function unweave(elem) {
-  var unwoven = elem.getElementsByClassName("unwoven")[0]
-  if (unwoven == undefined) {
-    throw new Error("No unwoven element found.");
+  for (var i = 0; i < elem.children.length; i++) {
+    var child = elem.children[i];
+    if (child.getAttribute("class") == "unwoven") {
+      return child.innerHTML;
+    }
   }
-  return unwoven.innerHTML;
+  throw new Error("[unweave] no unwoven span found in element: " + elem.innerHTML);
 }
 
 function weaveInlineChunk(chunk) {
@@ -34,7 +36,7 @@ function weaveInlineChunk(chunk) {
   /* Create code */
   var code = document.createElement("code");
   code.innerHTML = chunk.innerHTML;
-  /* replace HTML */
+  /* Replace HTML */
   chunk.innerHTML = null;
   chunk.appendChild(chunkHead);
   chunk.appendChild(code);
@@ -47,21 +49,26 @@ function weaveBlockChunk(chunk) {
   unwoven.setAttribute("class", "unwoven");
   unwoven.setAttribute("hidden", true);
   unwoven.innerHTML = chunk.innerHTML;
+
   /* Create chunk head span */
+  var tangleLink = document.createElement("a");
+  var chunkName = chunk.getAttribute("name");
+  tangleLink.setAttribute("href", "javascript:void(0)");
+  tangleLink.setAttribute("onclick", `tangle('${chunkName}')`);
+  tangleLink.innerHTML = chunkName;
   var chunkHead = document.createElement("span");
   chunkHead.setAttribute("class", "block-chunk-head");
   chunkHead.append(document.createTextNode("⟨"));
-  tangleLink = document.createElement("a");
-  var chunkName = chunk.getAttribute("name");
-  tangleLink.setAttribute("href", "#");
-  tangleLink.setAttribute("onclick", `tangle('${chunkName}')`);
-  tangleLink.innerHTML = chunkName;
   chunkHead.append(tangleLink);
   chunkHead.append(document.createTextNode("⟩≡"));
+
   /* Create pre */
   var pre = document.createElement("pre");
-  pre.innerHTML = chunk.innerHTML;
-  /* replace HTML */
+  var code = document.createElement("code");
+  code.innerHTML = chunk.innerHTML;
+  pre.appendChild(code);
+
+  /* Replace HTML */
   chunk.innerHTML = null;
   chunk.appendChild(chunkHead);
   chunk.appendChild(pre);
@@ -71,7 +78,7 @@ function weaveBlockChunk(chunk) {
 function weaveChunk(chunk) {
   if (!chunk.getAttribute("name")) {
     alert("ERROR 1");
-    throw new Error("Block chunk has no name");
+    throw new Error("[weaveChunk]: block chunk has no name");
   }
 
   if ((chunk.tagName == 'DIV') && (chunk.getAttribute("class") == "chunk")) {
@@ -96,14 +103,56 @@ function weave() {
     } else if (chunk.tagName == "DIV") {
       weaveBlockChunk(chunk);
     } else {
-      throw new Error("Unknown chunk type: " + chunk.tagName);
+      throw new Error("[weave] unknown chunk type: " + chunk.tagName);
     }
   }
 }
 
-function tangle(chunkName) {
-  alert("Not implemented yet!");
+function _tangle(chunkName) {
+  console.log("Tangling " + chunkName);
+  var chunks = document.getElementsByName(chunkName);
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+    console.log(chunk.getAttribute("export") == undefined);
+    unweave(chunk);
+  }
 }
 
+function __tangle(chunkName) {
+  var html = "";
 
-onload = weave;
+  console.log("Tangling " + chunkName);
+  var chunks = document.getElementsByName(chunkName);
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+    unweave(chunk);
+    /* Get the unwoven, which is always the last child */
+    var unwoven = chunk.children[chunk.children.length - 1];
+    console.log(unwoven);
+    for (var j = 0; j < unwoven.children.length; j++) {
+      var child = unwoven.children[j];
+      console.log(child);
+      if (child.getAttribute("class") == 'chunkref') {
+        var unwovenChunkRef = child.children[child.children.length - 1];
+        console.log(unwovenChunkRef.textContent);
+        html += _tangle(unwovenChunkRef.textContent);
+        console.log("111 " + html);
+      } else {
+        html += child.textContent;
+        console.log("222 " + html);
+      }
+    }
+  }
+  return html;
+}
+
+function tangle(chunkName) {
+  html = _tangle(chunkName);
+  // var tab = window.open('about:blank', '_blank');
+  // tab.document.write(html);
+  // tab.document.close();
+}
+
+onload = () => {
+  weave();
+}
