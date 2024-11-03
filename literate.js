efd8f8835999ed4f40dd048e94cd7b886f51cc81
shuiruge <shuiruge@whu.edu.cn>
2024-11-03 16:24:47 +0800

    Finish literate.js

--- literate.js
+++ literate.js
@@ -17,7 +17,7 @@ function unweave(elem) {
   for (var i = 0; i < elem.children.length; i++) {
     var child = elem.children[i];
     if (child.getAttribute("class") == "unwoven") {
-      return child.innerHTML;
+      return child;
     }
   }
   throw new Error("[unweave] no unwoven span found in element: " + elem.innerHTML);
@@ -75,6 +75,58 @@ function weaveBlockChunk(chunk) {
   chunk.appendChild(unwoven);
 }
 
+function regularizeCodeBlock(text) {
+  var result = "";
+  var indentation = 0;
+  var whitespaceCount = 0;
+  var counting = false;
+  for (var i = 0; i < text.length; i++) {
+    if ((result == "") && (text[i] == ' ')) {
+      indentation++;
+    }
+    else if ((result == "") && (text[i] == '\n')) {
+      indentation = 0;
+    }
+    else if (result == "") {
+      result += text[i];
+    }
+    else if (text[i] == '\n') {
+      whitespaceCount = 0;
+      counting = true;
+    }
+    else if ((counting == true) && (text[i] == ' ')) {
+      whitespaceCount++;
+    }
+    else if (counting == true) {
+      counting = false;
+      result += "\n";
+      if (whitespaceCount < indentation) throw new Error("Indentation error");
+      for (var j = 0; j < (whitespaceCount - indentation); j++) result += " ";
+      result += text[i];
+    }
+    else {
+      result += text[i];
+    }
+  }
+  return result;
+}
+
+function regularize() {
+  var divs = document.getElementsByTagName("div");
+  for (var i = 0; i < divs.length; i++) {
+    if (divs[i].getAttribute("class") == "chunk") {
+      divs[i].innerHTML = regularizeCodeBlock(divs[i].innerHTML);
+    }
+  }
+  var pres = document.getElementsByTagName("pre");
+  for (var i = 0; i < pres.length; i++) {
+    var codes = pres[i].getElementsByTagName("code");
+    for (var j = 0; j < codes.length; j++) {
+      codes[j].innerHTML = regularizeCodeBlock(codes[j].innerHTML);
+    }
+  }
+}
+
 function weaveChunk(chunk) {
   if (!chunk.getAttribute("name")) {
     alert("ERROR 1");
@@ -108,51 +160,112 @@ function weave() {
   }
 }
 
-function _tangle(chunkName) {
-  console.log("Tangling " + chunkName);
-  var chunks = document.getElementsByName(chunkName);
-  for (var i = 0; i < chunks.length; i++) {
-    var chunk = chunks[i];
-    console.log(chunk.getAttribute("export") == undefined);
-    unweave(chunk);
-  }
+function tangle(chunkName) {
+  var env = document.createElement("pre");
+  env.append(document.createElement("code"));
+  env.innerHTML = _tangle(chunkName);
+  var win = window.open('about:blank', '_blank');
+  win.document.write(env.outerHTML);
+  win.document.close();
 }
 
-function __tangle(chunkName) {
-  var html = "";
-
-  console.log("Tangling " + chunkName);
-  var chunks = document.getElementsByName(chunkName);
-  for (var i = 0; i < chunks.length; i++) {
-    var chunk = chunks[i];
-    unweave(chunk);
-    /* Get the unwoven, which is always the last child */
-    var unwoven = chunk.children[chunk.children.length - 1];
-    console.log(unwoven);
-    for (var j = 0; j < unwoven.children.length; j++) {
-      var child = unwoven.children[j];
-      console.log(child);
-      if (child.getAttribute("class") == 'chunkref') {
-        var unwovenChunkRef = child.children[child.children.length - 1];
-        console.log(unwovenChunkRef.textContent);
-        html += _tangle(unwovenChunkRef.textContent);
-        console.log("111 " + html);
-      } else {
-        html += child.textContent;
-        console.log("222 " + html);
-      }
+function getFirstChildByClass(node, className) {
+  for (var i = 0; i < node.children.length; i++) {
+    var child = node.children[i];
+    if (child.getAttribute("class") == className) {
+      return child;
     }
   }
-  return html;
+  return undefined;
 }
 
-function tangle(chunkName) {
-  html = _tangle(chunkName);
-  // var tab = window.open('about:blank', '_blank');
-  // tab.document.write(html);
-  // tab.document.close();
+function _tangle(chunkName) {
+  var code = "";
+  var chunks = document.getElementsByName(chunkName);
+  for (var i = 0; i < chunks.length; i++) {
+    if (chunks[i].getAttribute("class") != "chunk") continue;
+    var unwoven = unweave(chunks[i]).cloneNode(true);
+    /* Deal with chunk references */
+    var chunkRef = getFirstChildByClass(unwoven, "chunkref");
+    while (chunkRef != undefined) {
+      var subChunkName = unweave(chunkRef).innerHTML;
+      var subcode = _tangle(subChunkName);
+      /* Block chunk needs indentation */
+      if (chunks[i].tagName == "DIV") {
+        var indentation = getIndentation(chunkRef);
+        subcode = indent(subcode, indentation);
+      }
+      unwoven.replaceChild(document.createTextNode(subcode), chunkRef);
+      chunkRef = getFirstChildByClass(unwoven, "chunkref");
+    }
+    code += unescapeHTML(unwoven.innerHTML);
+    if (i < chunks.length - 1) code += "\n";
+  }
+  code = escapeHTML(code);
+  return code;
+}
+
+function debugText(text) {
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    if (text[i] == '\n') {
+      result += "\\n";
+    } else if (text[i] == '\t') {
+      result += "\\t";
+    } else if (text[i] == ' ') {
+      result += "\\s";
+    } else {
+      result += text[i];
+    }
+  }
+  return result;
+}
+
+function getTextBefore(node) {
+  var rangeBefore = document.createRange();
+  rangeBefore.setStart(node.parentNode.firstChild, 0);
+  rangeBefore.setEndBefore(node);
+  return rangeBefore.toString();
+}
+
+function getIndentation(chunkRef) {
+  var textBefore = getTextBefore(chunkRef);
+  var indentation = 0;
+  for (var i = textBefore.length - 1; i >= 0; i--) {
+    if (textBefore[i] == ' ') {
+      indentation++;
+    } else {
+      break;
+    }
+  }
+  return indentation;
+}
+
+function indent(text, indentation) {
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    result += text[i];
+    if (text[i] == '\n') {
+      for (var j = 0; j < indentation; j++) result += ' ';
+    }
+  }
+  return result;
+}
+
+function escapeHTML(text) {
+  return text.replace('>', "&gt;")
+             .replace('<', "&lt;");
+}
+
+function unescapeHTML(text) {
+  return text.replace(/&amp;/g, '&')
+             .replace(/&lt;/g, '<')
+             .replace(/&gt;/g, '>')
+             .replace(/&quot;/g, '"')
+             .replace(/&#39;/g, '\'');
 }
 
 onload = () => {
+  regularize();
   weave();
 }
